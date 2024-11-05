44b9675582756565a42feb881e68ea0cae1597e6
BrianJ-4 <brianjdev4@gmail.com>
2024-11-04 22:29:58 -0800

    Added contact page

--- src/page_modules/contact.js
+++ src/page_modules/contact.js
@@ -2,10 +2,23 @@ export function createContact() {
     // Get the content container
     const content = document.querySelector("#content");
 
+    // Create contact container
+    const contactContainer = document.createElement("div");
+    contactContainer.id = "contact-container";
+    
+    // Create contact text
+    const contactText = document.createElement("div");
+    contactText.id = "contact-text";
+    contactText.innerText = "123-456-7890"
+
     // Create icon element
     const iconElement = document.createElement("i");
     iconElement.className = "material-icons";
+    iconElement.id = "contact-icon";
     iconElement.textContent = "call";
 
-    content.appendChild(iconElement);
+    contactContainer.appendChild(iconElement);
+    contactContainer.appendChild(contactText);
+
+    content.appendChild(contactContainer);
 }
